bc6e9702bd15eefbc4efb1f9180c5a69b46e66ce
VCoklat <VCoklat@users.noreply.github.com>
2025-03-08 07:20:12 +0000

    return

--- configs.py
+++ configs.py
@@ -1,11 +1,8 @@
-
-
 save_dir                    = ''
 data_dir = {}
-data_dir['CUB']             = '/Few-Shot-Cosine-Transformer/dataset/CUB/' 
-data_dir['miniImagenet']    = '/Few-Shot-Cosine-Transformer/dataset/miniImagenet/' 
-data_dir['Omniglot']        = '/Few-Shot-Cosine-Transformer/dataset/Omniglot/' 
-data_dir['emnist']          = '/Few-Shot-Cosine-Transformer/dataset/emnist/' 
-data_dir['Yoga']            = '/Few-Shot-Cosine-Transformer/dataset/Yoga/'
-data_dir['CIFAR']           = '/Few-Shot-Cosine-Transformer/dataset/CIFAR_FS/'
-
+data_dir['CUB']             = '/kaggle/working/Few-Shot-Cosine-Transformer/dataset/CUB/' 
+data_dir['miniImagenet']    = '/kaggle/working/Few-Shot-Cosine-Transformer/dataset/miniImagenet/' 
+data_dir['Omniglot']        = '/kaggle/working/Few-Shot-Cosine-Transformer/dataset/Omniglot/' 
+data_dir['emnist']          = '/kaggle/working/Few-Shot-Cosine-Transformer/dataset/emnist/' 
+data_dir['Yoga']            = '/kaggle/working/Few-Shot-Cosine-Transformer/dataset/Yoga/'
+data_dir['CIFAR']           = '/kaggle/working/Few-Shot-Cosine-Transformer/dataset/CIFAR_FS/'
