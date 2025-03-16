66d26f527498ccbadaa5d4860ea02afdab26cde0
VCoklat <VCoklat@users.noreply.github.com>
2025-03-16 19:11:47 +0700

    0.9 False

--- methods/transformer.py
+++ methods/transformer.py
@@ -71,7 +71,7 @@ class FewShotTransformer(MetaTemplate):
         return acc, loss
 
 class Attention(nn.Module):
-    def __init__(self, dim, heads, dim_head, variant, initial_cov_weight=0.8, dynamic_weight=False):
+    def __init__(self, dim, heads, dim_head, variant, initial_cov_weight=0.9, dynamic_weight=False):
         super().__init__()
         inner_dim = heads * dim_head
         project_out = not(heads == 1 and dim_head == dim)
